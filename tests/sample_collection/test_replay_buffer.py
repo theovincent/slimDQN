af67b071125d7ef1d0ae7436800056cbaf1cd8f2
Yogesh Tripathi <yogeshtripathi@yogimac.local>
2024-02-13 15:43:36 +0100

    unit test for rb

--- tests/sample_collection/test_replay_buffer.py
+++ tests/sample_collection/test_replay_buffer.py
@@ -23,19 +23,39 @@ class TestReplayBuffer(unittest.TestCase):
                                gamma=0.99)
 
     def test_add(self):
-        pass
+        self.assertEqual(self.rb.is_empty(), True)
+        for i in range(50):
+            self.rb.add(observation=np.random.normal((2, 2)),
+                        action=[np.random.randint(0, 5)],
+                        reward=np.random.normal(),
+                        terminal=np.random.choice([True, False], p=[0.2, 0.8]))
+        self.assertEqual(self.rb.add_count, 50)
+        self.assertEqual(self.rb.is_empty(), False)
+        self.assertEqual(self.rb.is_full(), False)
+        for i in range(self.rb._replay_capacity):
+            self.rb.add(observation=np.random.normal((2, 2)),
+                        action=[np.random.randint(0, 5)],
+                        reward=np.random.normal(),
+                        terminal=np.random.choice([True, False], p=[0.2, 0.8]))
+        self.assertEqual(self.rb.is_full(), True)
 
-    def test_sample_index_batch(self):
-        pass
-    def test_sample_transition_batch(self):
-        pass
-    def test_is_empty(self):
-        pass
-    def test_is_full(self):
-        pass
-    def test_get_range(self):
-        pass
-    def test_is_valid_transition(self):
-        pass
+    def test_sample(self):
+        for i in range(self.rb._replay_capacity):
+            self.rb.add(observation=np.random.normal((2, 2)),
+                        action=[np.random.randint(0, 5)],
+                        reward=np.random.normal(),
+                        terminal=np.random.choice([True, False], p=[0.8, 0.2]))
+        batch_size = np.random.randint(1, 1000)
+        self.assertEqual(len(self.rb.sample_index_batch(batch_size)), batch_size)
+        self.assertEqual(len(self.rb.sample_transition_batch(batch_size)), 5)
+        self.assertEqual(len(self.rb.sample_transition_batch(batch_size)["observations"]), batch_size)
+        for i in range(self.rb._replay_capacity):
+            self.rb.add(observation=np.random.normal((2, 2)),
+                        action=[np.random.randint(0, 5)],
+                        reward=np.random.normal(),
+                        terminal=False,
+                        episode_end=True)
+        self.assertRaises(RuntimeError, self.rb.sample_index_batch, batch_size=1)
 
-
+    def test_horizon_based_buffer(self):
+        pass
