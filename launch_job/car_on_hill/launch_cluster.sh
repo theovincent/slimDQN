88b4f5f1a26f7fb7e63e072342ae581febd0744b
yogesh1q2w <yogesh120699@gmail.com>
2024-07-04 17:38:21 +0200

    better cluster args

--- launch_job/car_on_hill/launch_cluster.sh
+++ launch_job/car_on_hill/launch_cluster.sh
@@ -8,11 +8,11 @@ echo "launch train fqi"
 
 if [[ $GPU = true ]]
 then
-    submission_train_fqi="sbatch -J C_train_fqi --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=2 --mem-per-cpu=750M --time=2:00:00 --gres=gpu:1 -p stud,stud3080 \
+    submission_train_fqi="sbatch -J $EXPERIMENT_NAME\_fqi --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=1 --mem-per-cpu=750M --time=00:10:00 --gres=gpu:1 -p stud,stud3080 \
     --output=experiments/CarOnHill/out/$EXPERIMENT_NAME/FQI/train_fqi_%a.out \
     launch_job/car_on_hill/train_fqi.sh -e $EXPERIMENT_NAME $BASE_ARGS $FQI_ARGS -g"
 else
-    submission_train_fqi="sbatch -J C_train_fqi --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=8 --mem-per-cpu=750M --time=2:00:00 -p stud,stud3080 \
+    submission_train_fqi="sbatch -J $EXPERIMENT_NAME\_fqi --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=1 --mem-per-cpu=750M --time=00:15:00 -p stud,stud3080 \
     --output=experiments/CarOnHill/out/$EXPERIMENT_NAME/FQI/train_fqi_%a.out \
     launch_job/car_on_hill/train_fqi.sh -e $EXPERIMENT_NAME $BASE_ARGS $FQI_ARGS"
 fi
