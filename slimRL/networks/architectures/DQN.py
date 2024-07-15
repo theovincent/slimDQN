58a044a16753d038e3573caa81a959205a72fbcc
yogesh1q2w <yogesh120699@gmail.com>
2024-07-15 11:44:08 +0200

    condense DQNNet

--- slimRL/networks/architectures/DQN.py
+++ slimRL/networks/architectures/DQN.py
@@ -1,22 +1,13 @@
+from typing import Sequence
 import flax.linen as nn
 
-
 class DQNNet(nn.Module):
+    hidden_layers: Sequence[int]
     n_actions: int
-    hidden_layers: list
-
-    def setup(self):
-        self.initializer = nn.initializers.variance_scaling(scale=1.0, mode="fan_in", distribution="truncated_normal")
-        layers = []
-
-        for hidden_size in self.hidden_layers:
-            layers.append(nn.Dense(hidden_size, kernel_init=self.initializer))
-            layers.append(nn.relu)
-
-        layers.append(nn.Dense(self.n_actions, kernel_init=self.initializer))
-
-        self.network = nn.Sequential(layers)
 
     @nn.compact
-    def __call__(self, state):
-        return self.network(state)
+    def __call__(self, x):
+        for feature in self.hidden_layers:
+            x = nn.relu(nn.Dense(feature)(x))
+        x = nn.Dense(self.n_actions)(x)
+        return x
