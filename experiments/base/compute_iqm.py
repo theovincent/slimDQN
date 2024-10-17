a3952cda1753f8ea4141750aa107a9d9e11579dc
yogesh1q2w <yogesh120699@gmail.com>
2024-10-17 21:12:45 +0200

    iqm: bug fix

--- experiments/base/compute_iqm.py
+++ experiments/base/compute_iqm.py
@@ -6,34 +6,13 @@ import scipy.stats
 import numpy as np
 
 
-# def compute_iqm_conf(array: np.ndarray, n_seeds: int, n_bootstraps: int):
-#     iqm = scipy.stats.trim_mean(np.sort(array), proportiontocut=0.25)
-
-#     bootstrap_iqms = np.zeros(n_bootstraps)
-#     for i in range(n_bootstraps):
-#         vals = np.random.choice(array, size=n_seeds)
-#         bootstrap_iqms[i] = scipy.stats.trim_mean(np.sort(vals), proportiontocut=0.25)
-
-#     confs = np.percentile(bootstrap_iqms, [2.5, 97.5])
-
-#     return iqm, confs
-
-
 def get_iqm_and_conf_per_epoch(array: jnp.ndarray, n_bootstraps: int = 2000):
     n_seeds, n_epochs = array.shape
     if n_seeds == 1:
         return array.reshape(-1), np.stack([array.reshape(-1), array.reshape(-1)])
 
-    # with Pool(n_epochs) as pool:
-    #     results = pool.starmap(
-    #         compute_iqm_conf,
-    #         [(array[:, epoch], n_seeds, n_bootstraps) for epoch in range(n_epochs)],
-    #     )
-
-    # iqms, confs = zip(*results)
-
     key = jax.random.key(seed=0)
-    iqm = scipy.stats.trim_mean(jnp.sort(array, axis=-1), proportiontocut=0.25, axis=-1)
+    iqm = scipy.stats.trim_mean(jnp.sort(array, axis=0), proportiontocut=0.25, axis=0)
 
     bootstrap_key, key = jax.random.split(key=key)
     bootstrap_keys = jax.random.split(bootstrap_key, num=n_epochs)
@@ -41,7 +20,7 @@ def get_iqm_and_conf_per_epoch(array: jnp.ndarray, n_bootstraps: int = 2000):
         lambda k, row: jax.random.choice(k, row, shape=(n_bootstraps, n_seeds)), in_axes=(0, 1)
     )(bootstrap_keys, array)
 
-    bootstrap_samples = scipy.stats.trim_mean(jnp.sort(bootstrap_samples, axis=-1), proportiontocut=0.25, axis=-1)
-    confs = jnp.percentile(bootstrap_samples, q=[2.5, 97.5], axis=-1)
+    bootstrap_samples = scipy.stats.trim_mean(jnp.sort(bootstrap_samples, axis=-1), proportiontocut=0.25, axis=1)
+    confs = jnp.percentile(bootstrap_samples, q=jnp.array([2.5, 97.5]), axis=1)
 
     return np.array(iqm), np.array(confs)
