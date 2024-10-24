c1f79384a3b5b44d48eef226ce70114f41a7b230
yogesh1q2w <yogesh120699@gmail.com>
2024-10-24 11:51:32 +0200

    iqm: verified ag. rliable, n_games extended

--- experiments/base/compute_iqm.py
+++ experiments/base/compute_iqm.py
@@ -1,26 +1,34 @@
-# from multiprocessing import Pool
-
 import jax
 import jax.numpy as jnp
 import scipy.stats
 import numpy as np
 
 
-def get_iqm_and_conf_per_epoch(array: jnp.ndarray, n_bootstraps: int = 2000):
-    n_seeds, n_epochs = array.shape
-    if n_seeds == 1:
+def get_iqm_and_conf_per_epoch(array: np.ndarray, n_bootstraps: int = 2000):
+    n_seeds, n_tasks, n_epochs = array.shape
+    if n_tasks == 1 and n_seeds == 1:
         return array.reshape(-1), np.stack([array.reshape(-1), array.reshape(-1)])
 
     key = jax.random.key(seed=0)
-    iqm = scipy.stats.trim_mean(jnp.sort(array, axis=0), proportiontocut=0.25, axis=0)
+    iqm = jnp.array(
+        [scipy.stats.trim_mean(array[..., epoch], proportiontocut=0.25, axis=None) for epoch in range(n_epochs)]
+    )
 
     bootstrap_key, key = jax.random.split(key=key)
-    bootstrap_keys = jax.random.split(bootstrap_key, num=n_epochs)
+    bootstrap_keys = jax.random.split(bootstrap_key, num=n_epochs * n_tasks).reshape((n_epochs, n_tasks))
     bootstrap_samples = jax.vmap(
-        lambda k, row: jax.random.choice(k, row, shape=(n_bootstraps, n_seeds)), in_axes=(0, 1)
+        lambda k, a: jnp.array(
+            [
+                jax.random.choice(key=k[idx_task], a=a[..., idx_task], shape=(n_seeds, n_bootstraps))
+                for idx_task in range(n_tasks)
+            ]
+        ),
+        in_axes=(0, -1),
     )(bootstrap_keys, array)
 
-    bootstrap_samples = scipy.stats.trim_mean(jnp.sort(bootstrap_samples, axis=-1), proportiontocut=0.25, axis=1)
+    bootstrap_samples = bootstrap_samples.reshape((n_epochs, -1, n_bootstraps))
+
+    bootstrap_samples = scipy.stats.trim_mean(bootstrap_samples, proportiontocut=0.25, axis=1)
     confs = jnp.percentile(bootstrap_samples, q=jnp.array([2.5, 97.5]), axis=1)
 
-    return np.array(iqm), np.array(confs)
+    return iqm, confs
