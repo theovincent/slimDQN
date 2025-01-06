ef90438af4e9058512dc8550c928942df40e3704
yogesh1q2w <yogesh120699@gmail.com>
2025-01-06 22:45:00 +0100

    remove hardcoded network

--- slimdqn/networks/architectures/dqn.py
+++ slimdqn/networks/architectures/dqn.py
@@ -34,28 +34,3 @@ class DQNNet(nn.Module):
             x = nn.relu((nn.Dense(self.features[idx_layer], kernel_init=initializer)(x)))
 
         return nn.Dense(self.n_actions, kernel_init=initializer)(x)
-
-
-class DQNNetNature(nn.Module):
-    n_actions: int
-
-    @nn.compact
-    def __call__(self, x):
-        initializer = nn.initializers.variance_scaling(scale=1.0, mode="fan_avg", distribution="truncated_normal")
-        x = nn.relu(
-            nn.Conv(features=32, kernel_size=(8, 8), strides=(4, 4), kernel_init=initializer)(
-                jnp.array(x, ndmin=4) / 255.0
-            )
-        )
-        x = nn.relu(
-            nn.Conv(features=64, kernel_size=(4, 4), strides=(2, 2), kernel_init=initializer)(x)
-        )
-        x = nn.relu(
-            nn.Conv(features=64, kernel_size=(3, 3), strides=(1, 1), kernel_init=initializer)(x)
-        )
-        x = x.reshape((x.shape[0], -1))
-        x = jnp.squeeze(x)
-
-        x = nn.relu((nn.Dense(512, kernel_init=initializer)(x)))
-
-        return nn.Dense(self.n_actions, kernel_init=initializer)(x)
--- slimdqn/sample_collection/utils.py
+++ slimdqn/sample_collection/utils.py
@@ -7,21 +7,21 @@ from functools import partial
 from slimdqn.sample_collection.replay_buffer import ReplayBuffer, TransitionElement
 
 
-@partial(jax.jit, static_argnames=("q", "n_actions", "epsilon_fn"))
-def select_action(q, params, state, key, n_actions, epsilon_fn, n_training_steps):
+@partial(jax.jit, static_argnames=("best_action_fn", "n_actions", "epsilon_fn"))
+def select_action(best_action_fn, params, state, key, n_actions, epsilon_fn, n_training_steps):
     uniform_key, action_key = jax.random.split(key)
     return jnp.where(
         jax.random.uniform(uniform_key) <= epsilon_fn(n_training_steps),  # if uniform < epsilon,
         jax.random.randint(action_key, (), 0, n_actions),  # take random action
-        jnp.argmax(q.apply(params, state)),  # otherwise, take a greedy action
+        best_action_fn(params, state),  # otherwise, take a greedy action
     )
 
 
 def collect_single_sample(key, env, agent, rb: ReplayBuffer, p, epsilon_schedule, n_training_steps: int):
     time_being_action_selection = time.time()
     action = select_action(
-        agent.q_network, agent.params, agent.state, key, env.n_actions, epsilon_schedule, n_training_steps
-    )
+        agent.best_action, agent.params, agent.state, key, env.n_actions, epsilon_schedule, n_training_steps
+    ).item()
     jax.block_until_ready(action)
     time_action_selection = time.time() - time_being_action_selection
 
