f69cc613739010aa40e66e38ff9cb3c92057d5cc
Habibi <h.maraqten@gmail.com>
2026-02-04 11:57:16 +0100

    update launch file for mountain car

--- launch_job/mountain_car/launch.sh
+++ launch_job/mountain_car/launch.sh
@@ -1,46 +1,40 @@
 #!/bin/bash
-
-#policy expertise
-EXPERTISE="rookie"
-FEATURES="5 5"
-BATCH_SIZE=6
-
 ENV_NAME="mountain_car"
-ALGO="dqn"
 
-EXPERIMENT_TAG="${EXPERTISE}_${ALGO}_${ENV_NAME}"
-
+FEATURES="5"
+BATCH_SIZE=32
+RB=10000
+NT=10000
+TUP=200
 
 FIRST_SEED=0
 LAST_SEED=5
 N_PARALLEL_SEEDS=1
 
-
-BASE_ARGS="--architecture_type fc \
-    --replay_buffer_capacity 10000 \
+ARGS="--architecture_type fc \
+    --replay_buffer_capacity $RB \
+    --n_training_steps_per_epoch $NT \
+    --features $FEATURES $FEATURES \
+    --batch_size $BATCH_SIZE \
     --update_horizon 1 \
-    --horizon 250 \
+    --horizon 200 \
     --gamma 0.99 \
     --learning_rate 3e-3 \
-    --n_epochs 30 \
-    --target_update_period 200 \
-    --n_states_1 17 \
-    --n_states_2 17 \
+    --n_epochs 20 \
+    --target_update_period $TUP \
     "
+EXPERIMENT_TAG="DQN_F${FEATURES}_RB${RB}_NT${NT}_T${TUP}_${ENV_NAME}"
 
 ####################################################################################
-POLICY_EXPERTISE_ARGS="--features $FEATURES --batch_size $BATCH_SIZE"
 
 
 LAUNCHER_SCRIPT="launch_job/${ENV_NAME}/cluster_${ALGO}.sh"
-FULL_EXPERIMENT_NAME="${EXPERIMENT_TAG}"
 
 echo "----------------------------------------------------------------"
 echo "Launching Job"
-echo "Experiment: $FULL_EXPERIMENT_NAME"
+echo "Experiment: $EXPERIMENT_TAG"
 echo "Algorithm:  $ALGO"
 echo "Policy Expertise Args: $EXPERTISE"
-echo "Data Args: $DATA_ARGS"
 echo "----------------------------------------------------------------"
 
 bash $LAUNCHER_SCRIPT \
@@ -48,4 +42,4 @@ bash $LAUNCHER_SCRIPT \
     --first_seed $FIRST_SEED \
     --last_seed $LAST_SEED \
     --n_parallel_seeds $N_PARALLEL_SEEDS \
-    $BASE_ARGS $POLICY_EXPERTISE_ARGS $DATA_ARGS
+    $ARGS 
